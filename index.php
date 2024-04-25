1a024432a0928ed34f9a992026afb8575418e555
Kim Ji Yu <spbabo97@naver.com>
2024-04-26 00:21:31 +0900

    :lipstick: UI 작성

--- index.php
+++ index.php
@@ -13,11 +13,51 @@
         
             <section class="section01">
 
-                <div class="init">
+                <div class="init"></div>
+
+                <h1>Light up, your life</h1>
+
+                <div class="up_scale">
+                    
+                    <div class="scrollDown">
+                        <div class="text"></div>
+                        <div class="icon">
+                            <img src="/asset/image/main/down.png" alt="">
+                        </div>
+                    </div>
 
                 </div>
 
-                <h1>Light up, your life </h1>
+            </section>
+
+            <section class="section02"></section>
+
+            <section class="section03"></section>
+
+            <section class="section04">
+                <div class="tbx">
+                    <p>
+                        꿈꾸는 세상을 구현하는 DISPLAY LEADER 기업<br/>
+                        디스플레이의 새로운 차원을 경험하다.
+                    </p>
+                    <p>
+                        꿈꾸는 세상을 구현하는 DISPLAY LEADER 기업<br/>
+                        디스플레이의 새로운 차원을 경험하다.
+                    </p>
+                </div>
+            </section>
+
+            <section class="section05"></section>
+
+            <section class="section06">
+
+                <div class="tbx">
+                    <dl>
+                        <dt>DISPLAY 전문기업, JBDISPLAY 와 함께 해보세요!</dt>
+                        <dd>Let’s get in touch</dd>
+                    </dl>
+                    <a href="">견적문의 <i class="xi-angle-down"></i></a>
+                </div>
 
             </section>
 

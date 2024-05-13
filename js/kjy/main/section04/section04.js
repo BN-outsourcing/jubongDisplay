3f266d9cf39a5a0e19efbe73a770958ac1911b6d
Kim Ji Yu <spbabo97@naver.com>
2024-05-14 03:47:05 +0900

    :bug: 파트너 부분을 수정했습니다.

--- js/kjy/main/section04/section04.js
+++ js/kjy/main/section04/section04.js
@@ -24,18 +24,19 @@ export default ()=>{
         const tl = gsap.timeline({
             scrollTrigger : {
                 trigger : "._main .section04",
-                start : "top center",
+                start : "top top+=25%",
+                end : "bottom bottom",
                 // markers : true,
                 // pin : true,
                 // pinType : isDesktop ? "transform" : "fixed",
-                // scrub : true,
                 // end : "+=300%"
             }
         });
     
         $('._main .section04 .tbx.pc p span').each((i,e)=>{
             tl.from(e,{
-                clipPath: "inset(0% 100% 0% 0%)"
+                clipPath: "inset(0% 100% 0% 0%)",
+                duration : 1.3,
             },i === 0 ? "a" : "a+=75%")
         });
 
@@ -46,18 +47,19 @@ export default ()=>{
         const tl = gsap.timeline({
             scrollTrigger : {
                 trigger : "._main .section04",
-                start : "top center",
+                start : "top top+=25%",
+                end : "bottom bottom",
                 // pin : true,
                 // pinType : isDesktop ? "transform" : "fixed",
-                // scrub : true,
                 // end : "+=300%"
             }
         });
     
         $('._main .section04 .tbx.mob p span').each((i,e)=>{
             tl.from(e,{
+                duration : 0.8,
                 clipPath: "inset(0% 100% 0% 0%)"
-            })
+            },">-=25%")
         });
 
     });
--- js/kjy/main/section05/section05.js
+++ js/kjy/main/section05/section05.js
@@ -23,7 +23,9 @@ export default ()=>{
     const tl = gsap.timeline({
         scrollTrigger : {
             trigger : "._main .section05 .tbx",
+            endTrigger : "._main .section05",
             start : "center center",
+            end : "bottom bottom",
             pin : true,
             pinSpacing : false,
             scrub : 1,
@@ -36,134 +38,56 @@ export default ()=>{
         },i === 0 ? 'c' : "c+=50%");
     });
 
-    // console.log('쉣');
 
-    /* const tl2 = gsap.timeline({
+    gsap.timeline({
         scrollTrigger : {
             trigger : "._main .section05",
-            markers : true,
-            start : "top center",
-            end : "bottom center",
-            scrub : 1,
-        }
-    }); */
-
-    gsap.to('._main .section05 .h-grid .grid',{
-        z : 5000,
-        scrollTrigger : {
-            trigger : "._main .section05 .h-grid",
             endTrigger : "._main .section05",
             // markers : true,
-            start : "center center",
-            end : "bottom center",
+            start : "top center",
+            end : "bottom top",
             scrub : 1,
         }
-    });
-
-    /* $('._main .section05 .grid .col').each((_,e)=>{
-    
-        tl2.to(e,{
-            rotateX : 0,
-            scaleY : 1,
-            duration : 1.5,
-            ease : "none"
-        },'a');
-
-    }); */
-
-    
-
-    const mm = gsap.matchMedia();
-
-    // mm.add({
-    //     isDesktop,
-    //     isMobile
-    // },(context)=>{
-
-    //     // const {isDesktop,isMobile} = context.conditions;
-
-    //     /* $('._main .section05 .grid .col').each((_,e)=>{
-
-    //         gsap.set(e,{
-    //             z : 'random(-30, -5000)',
-    //             rotateX : 'random(-10, -60)',
-    //             scaleY : 'random(1, 1.2)',
-    //             xPercent : `random(-100, 100)`,
-    //             yPercent : `random(-100, 100)`,
-    //             // transformOrigin: "33% 0"
-    //         })
-    
-    //     }); */
-    
-    //     /* const ab = gsap.timeline({
-    //         scrollTrigger : {
-    //             trigger : "._main .section05",
-    //             start : "top-=15% center",
-    //             end : "bottom bottom",
-    //             scrub : true,
-    //             // markers : true
-    //         }
-    //     }) */
-    
-
-    //     /* const tl = gsap.timeline({
-    //         scrollTrigger : {
-    //             trigger : "._main .section05",
-    //             start : "top top",
-    //             end : "+=400%",
-    //             pin : true,
-    //             scrub : 1,
-    //             // invalidateOnRefresh : true,
-    //             // start : isDesktop ? "top top+=25%" : "top top",
-    //             // end : "bottom center",
-                
-    //             // pinSpacing : true,
-                
-    //             // markers : true
-    //         }
-    //     });
-
-    //     $('._main .section05 .tbx dd .p span').each((i,e)=>{
-    //         tl.to(e,{
-    //             clipPath: "inset(0% 0% 0% 0%)"
-    //         },i === 0 ? 'c' : "c+=50%");
-    //     })
-
-    //     tl.fromTo('._main .section05 .grid',{
-    //         yPercent : 50,
-    //         z : -1500
-    //     },{
-    //         yPercent : 0,
-    //         z : 0,
-    //         duration : 1.5
-    //     },'c');
-        
-    //     $('._main .section05 .grid .col').each((_,e)=>{
-    
-    //         tl.to(e,{
-    //             rotateX : 0,
-    //             scaleY : 1,
-    //             xPercent : 0,
-    //             yPercent : 0,
-    //             duration : 1.5,
-    //             ease : "none"
-    //         },'a+=20%');
-    
-    //     });
-    
-    //     tl.fromTo('._main .section05 .grid',{
-    //         z : 0,
-    //         yPercent : 0,
-    //         scale : 1
-    //     },{
-    //         z : 7000,
-    //         yPercent : -20,
-    //         scale : 1.2,
-    //         duration : 1.5,
-    //         ease : "none"
-    //     },'a+=20%'); */
-
-    // })
-
+    })
+    .to('._main .section05 .h-grid .grid .col',{
+        x : (index, target, targets)=>{
+            if(gsap.getProperty(target,"x") > 0){
+                return "+=random(50%, 150%)"
+            }else{
+                return "-=random(50%, 150%)"
+            }
+        },
+        y : (index, target, targets)=>{
+            if(gsap.getProperty(target,"y") > 0){
+                return "+=random(50%, 150%)"
+            }else{
+                return "-=random(50%, 150%)"
+            }
+        }
+    },'a')
+    .to('._main .section05 .h-grid .grid .col',{
+        x : (index, target, targets)=>{
+            if(gsap.getProperty(target,"x") > 0){
+                return "+=random(50%, 150%)"
+            }else{
+                return "-=random(50%, 150%)"
+            }
+        },
+        y : (index, target, targets)=>{
+            if(gsap.getProperty(target,"y") > 0){
+                return "+=random(50%, 150%)"
+            }else{
+                return "-=random(50%, 150%)"
+            }
+        }
+    },'a+=25%')
+    .from('._main .section05 .h-grid .grid .col',{
+        opacity : 0.5
+    },'a')
+    .to('._main .section05 .h-grid .grid',{
+        z : 7000,
+        xPercent : -25,
+        yPercent : -15,
+    },"a+=25%")
 
 }
